e1a48f69b136ee8368aa62ff0324bcb6b66a3f21
shkhan9 <shehatagamal120@yahoo.com>
2018-01-29 05:20:51 -0700

    first commit

---
+++ aqqqaq/qw.swift
@@ -0,0 +1,21 @@
+//
+//  qw.swift
+//  aqqqaq
+//
+//  Created by sameh mohammed on 1/29/18.
+//  Copyright © 2018 com.trio. All rights reserved.
+//
+
+import UIKit
+
+class qw: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ aqqqaq/rtte.swift
@@ -0,0 +1,21 @@
+//
+//  rtte.swift
+//  aqqqaq
+//
+//  Created by sameh mohammed on 1/29/18.
+//  Copyright © 2018 com.trio. All rights reserved.
+//
+
+import UIKit
+
+class rtte: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
